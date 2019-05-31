3a2d1b4ef469ed6ce98ed77af5eac5556345426a
MagicTurtle <raffertychen@gmail.com>
2019-05-30 17:33:50 -0700

    added reply functionality

--- mail/mail.py
+++ mail/mail.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import re
+import templates
 
 from email.mime.text import MIMEText
 from googleapiclient.discovery import build
@@ -9,19 +10,16 @@ from oauth2client import file, client, tools
 from pathlib import Path
 
 
+ABS_PATH = Path().resolve() / 'mail'
 SCOPES = 'https://www.googleapis.com/auth/gmail.modify'
 
-REPLY_TEMPLATE = "Hello, {}!\r\n\r\nThank you for sending me mail! 😊\r\nI'm currently in development, so this is all I can reply with. 😔\r\nBut come back later and there will probably be more stuff then!"
-SARAH_TEMPLATE = "Hello, {}!\r\n\r\nThank you for sending me mail! 😊\r\nI'm currently in development, so this is all I can reply with. 😔\r\nBut come back later and there will probably be more stuff then!\r\n\r\nBTW, Raff says he loves you. 😍"
 
 def get_credentials():
-    abs_path = Path().resolve() / 'mail'
-
-    store = file.Storage(abs_path / 'token.json')
+    store = file.Storage(ABS_PATH / 'token.json')
     creds = store.get()
 
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets(abs_path / 'client_secrets.json', SCOPES)
+        flow = client.flow_from_clientsecrets(ABS_PATH / 'client_secrets.json', SCOPES)
         creds = tools.run_flow(flow, store)
         store.put(creds)
     
@@ -39,19 +37,30 @@ def get_mail(service):
             yield msg
 
 
-def create_mail(to: str, subject: str, message_text: str, reply_to: str = None) -> {}:
+def create_mail(to: str, subject: str, message_text: str, reply_to: {str: str} = None) -> {}:
     message = MIMEText(message_text)
     message['to'] = to
     message['from'] = 'emailchessraff@gmail.com'
     message['subject'] = subject
 
+    if reply_to != {}:
+        message['threadId'] = reply_to['threadId']
+        message['Message-ID'] = reply_to['Message-ID']
+        
+        if 'In-Reply-To' in reply_to:
+            message['In-Reply-To'] = reply_to['In-Reply-To']
+        else:
+            message['In-Reply-To'] = reply_to['Message-ID']
+
+        if 'References' in reply_to:
+            message['References'] = reply_to['References']
+        else:
+            message['References'] = reply_to['Message-ID']
+
     b64_bytes = base64.urlsafe_b64encode(message.as_bytes())
     b64_string = b64_bytes.decode()
 
     raw_message = {'raw': b64_string}
-    
-    if reply_to:
-        raw_message['threadId'] = reply_to
 
     return raw_message
 
@@ -63,16 +72,29 @@ def main():
     for msg in get_mail(service):
         threadId = msg['threadId']
 
+        references = ''
+        in_reply_to = ''
+
         for header in msg['payload']['headers']:
             if header['name'] == 'From':
                 name, _, addr = re.search(r'(?:(\w+)\s+(\w+))\s+<(\w+@\w+\.\w+)>', header['value']).groups()
             elif header['name'] == 'Subject':
                 subject = header['value']
+            elif header['name'] == 'Message-ID':
+                message_id = header['value']
+            elif header['name'] == 'References':
+                references = header['value']
+            elif header['name'] == 'In-Reply-To':
+                in_reply_to = header['value']
         
-        if addr == json.load(open('sarah.json', 'r'))['email']:
-            new_msg = create_mail(addr, subject, SARAH_TEMPLATE.format(name), threadId)
-        else:   
-            new_msg = create_mail(addr, subject, REPLY_TEMPLATE.format(name), threadId)
+        reply_dict = {'threadId': threadId, 'Message-ID': message_id}
+
+        if references != '':
+            reply_dict['References'] = references
+        if in_reply_to != '':
+            reply_dict['In-Reply-To'] = in_reply_to
+
+        new_msg = create_mail(addr, subject, templates.REPLY.format(name), reply_dict)
         service.users().messages().send(userId = 'me', body = new_msg).execute()
     
 
---
+++ mail/templates.py
@@ -0,0 +1,8 @@
+REPLY = """Hello, {}!
+
+Thank you for sending me mail! 😊
+I'm currently in development, so this is all I can reply with. 😔
+But come back later and there will probably be more stuff then!
+"""
+
+START_GAME = """"""
